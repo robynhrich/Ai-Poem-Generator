12f4e2320231eb563433bafcebaebdbbafbdfb72
Robyn H Richardson <robynhrich@icloud.com>
2023-12-19 14:50:57 -0500

    created an image folder

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="blink">⏳ Generating happy feelings for you about ${instructionsInput.value} ☺️</div>`;
+  poemElement.innerHTML = `<div class="blink">⏳</div> Generating happy feelings for you about ${instructionsInput.value} ☺️`;
 
   axios.get(apiUrl).then(displayPoem);
 }
