84c5f97668620454e715a965d24c318445eb2608
Robyn H Richardson <robynhrich@icloud.com>
2023-12-19 13:54:40 -0500

    Fixed JS error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-function(event){
+function generatePoem(event) {
+  event.preventDefault();
 
-  new Typewriter('#poem', {
-  strings: "Happy feelings in the air",
-  autoStart: true,
-  delay: 1,
-  cursor:"",
-  
-
-});
- 
+  new Typewriter("#poem", {
+    strings: "Happy feelings in the air",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
-
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
